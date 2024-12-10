bd46f3072c64804e9f4bebe74cbd22b99d77bb03
Marina Tobler <marina.tobler@wemakesoftware.eu>
2024-12-10 18:10:06 +0200

    lombok annotations are deleted from user

--- src/main/java/com/todo/models/User.java
+++ src/main/java/com/todo/models/User.java
@@ -4,13 +4,31 @@ package com.todo.models;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class User {
     String name;
     String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
