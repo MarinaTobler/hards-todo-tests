87ce27d8b40a6a6db941242acc3b68b90daa41f8
Marina Tobler <marina.tobler@wemakesoftware.eu>
2024-12-15 17:04:49 +0200

    duplicating new TodoRequest() is deleted, returned correct Responses

--- src/main/java/com/todo/requests/ValidatedTodoRequest.java
+++ src/main/java/com/todo/requests/ValidatedTodoRequest.java
@@ -7,59 +7,66 @@ import org.apache.http.HttpStatus;
 
 import java.util.List;
 
+// ?? .asserThat() в реквестах не надо, т.к. здесь это не проверки? проверяем в тестах?
+// ?? ContentType не надо здесь прописывать? т.к. это устанавливается в RequestSpec?
 public class ValidatedTodoRequest extends Request implements CrudInterface<Todo>, SearchInterface {
+    private TodoRequest todoRequest;
+
+    // NB! сразу здесь создаём новый todoRequest, чтобы потом не повторяться в каждом запросе:
     public ValidatedTodoRequest(RequestSpecification reqSpec) {
         super(reqSpec);
+        todoRequest = new TodoRequest(reqSpec);
     }
 
-    // ?? как и где проверять, что response body is emptyOrNullString?
-//    @Override
-//    public Object create(Todo entity) {
-//        return new TodoRequest(reqSpec).create(entity)
-//                .then().assertThat().statusCode(HttpStatus.SC_CREATED)
-//                .extract().as(Todo.class);
-//    }
-
-//    @Override
-//    public String create(Todo entity) {
-//        return new TodoRequest(reqSpec).create(entity)
-//                .then().assertThat().statusCode(HttpStatus.SC_CREATED)
-//                .extract().asString();
-//    }
+    // Как и где проверять, что response body is emptyOrNullString? Ответ: т.к. по конвенции create должен возвращать 201 и
+    // пустую строку, то прямо здесь десерелизуем в строку и потом в тесте проверяем, что она пустая:
 
     @Override
-    public Object create(Todo entity) {
-        return new TodoRequest(reqSpec).create(entity)
-                .then().assertThat()
-                .statusCode(HttpStatus.SC_CREATED);
-    }
-
-    @Override
-    public Todo update(long id, Todo entity) {
-        return new TodoRequest(reqSpec).update(id, entity)
-                .then().assertThat().statusCode(HttpStatus.SC_OK)
-                .contentType(ContentType.JSON)
-                .extract().as(Todo.class);
-    }
-
-    @Override
-    public String delete(long id) {
-        return new TodoRequest(reqSpec).delete(id)
-                .then().assertThat().statusCode(HttpStatus.SC_NO_CONTENT)
+    public String create(Todo entity) {
+        return todoRequest.create(entity)
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED)
                 .extract().asString();
     }
 
+    // сразу десерелизуем в класс
     @Override
-    public Todo[] readAll() {
-        return new TodoRequest(reqSpec).readAll()
-                .then().assertThat().statusCode(HttpStatus.SC_OK)
-                .extract().as(Todo[].class);
+    public Todo update(long id, Todo entity) {
+        return todoRequest.update(id, entity)
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract().as(Todo.class);
     }
 
+    // т.к. ответ должен быть пустой, то десерелизуем в строку, и проверяем, что она пустая
     @Override
-    public Todo[] readAll(int offset, int limit) {
-        return new TodoRequest(reqSpec).readAll()
-                .then().assertThat().statusCode(HttpStatus.SC_OK)
-                .extract().as(Todo[].class);
+    public String delete(long id) {
+        return todoRequest.delete(id)
+                .then()
+                .statusCode(HttpStatus.SC_NO_CONTENT)
+                .extract()
+                .body()
+                .asString();
     }
+
+    // Возвращаем сразу List, а не массив, т.к. с ним удобнее потом работать: м. использовать .stream, .add и т.д.
+    @Override
+    public List<Todo> readAll(int offset, int limit) {
+        Todo[] todos = todoRequest.readAll()
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract().as(Todo[].class);
+        return List.of(todos);
+    }
+
+    public List<Todo> readAll() {
+        Todo[] todos = todoRequest.readAll()
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract().as(Todo[].class);
+        return List.of(todos);
+    }
+
+
 }
