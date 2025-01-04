05c3c3df092d6b4b499dea579ac0a1e293621d66
Marina Tobler <marina.tobler@wemakesoftware.eu>
2025-01-04 19:00:52 +0200

    @Step added

--- src/main/java/com/todo/requests/TodoRequest.java
+++ src/main/java/com/todo/requests/TodoRequest.java
@@ -3,6 +3,7 @@ package com.todo.requests;
 import com.todo.models.Todo;
 import com.todo.requests.interfaces.CrudInterface;
 import com.todo.requests.interfaces.SearchInterface;
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
@@ -16,6 +17,7 @@ public class TodoRequest extends Request implements CrudInterface<Todo>, SearchI
     }
 
     @Override
+    @Step("Create {entity}")
     public Response create(Todo entity) {
         return given()
                 .spec(reqSpec)
@@ -25,6 +27,7 @@ public class TodoRequest extends Request implements CrudInterface<Todo>, SearchI
     }
 
     @Override
+    @Step("Update {entity}")
     public Response update(long id, Todo entity) {
         return given()
                 .spec(reqSpec)
@@ -34,18 +37,11 @@ public class TodoRequest extends Request implements CrudInterface<Todo>, SearchI
     }
 
     @Override
+    @Step("Delete {id}")
     public Response delete(long id) {
         return given()
                 .spec(reqSpec)
                 .delete(new Endpoint(TODO_ENDPOINT).build() + id);
-    }
-
-    @Override
-    public Response readAll() {
-        return given()
-                .spec(reqSpec)
-                .when()
-                .get(new Endpoint(TODO_ENDPOINT).build());
     }
 
 //    @Override
@@ -58,11 +54,21 @@ public class TodoRequest extends Request implements CrudInterface<Todo>, SearchI
 // лучше переписать как ниже:
 
     @Override
+    @Step("Read all by {offset, limit}")
     public Response readAll(int offset, int limit) {
         return given()
                 .spec(reqSpec)
                 .queryParam("offset", offset)
                 .queryParam("limit", limit)
+                .when()
+                .get(new Endpoint(TODO_ENDPOINT).build());
+    }
+
+    @Override
+    @Step("Read all")
+    public Response readAll() {
+        return given()
+                .spec(reqSpec)
                 .when()
                 .get(new Endpoint(TODO_ENDPOINT).build());
     }
